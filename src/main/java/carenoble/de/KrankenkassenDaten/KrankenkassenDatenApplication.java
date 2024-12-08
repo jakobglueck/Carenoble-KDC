730afa975bf5f96d6de16b5b8beadf26b43bf16f
Jakob Glück <jakob.glueck4@gmail.com>
2024-12-08 19:41:49 +0100

    Initial commit

---
+++ src/main/java/carenoble/de/KrankenkassenDaten/KrankenkassenDatenApplication.java
@@ -0,0 +1,13 @@
+package carenoble.de.KrankenkassenDaten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrankenkassenDatenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrankenkassenDatenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/carenoble/de/KrankenkassenDaten/KrankenkassenDatenApplicationTests.java
@@ -0,0 +1,13 @@
+package carenoble.de.KrankenkassenDaten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrankenkassenDatenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
